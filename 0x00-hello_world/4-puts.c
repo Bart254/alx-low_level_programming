bb3d9b7747db669e7a237cc006da133d569420bf
Bart254 <omondibartlett3@gmail.com>
2022-10-14 12:07:33 -0700

    Printing command

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*
+*prints encased words
+*main - Entry point
+*Return- always zeo is success
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
+}
