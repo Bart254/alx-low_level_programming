d265e8569d1ef104d26b24dca36286b376c5aec7
Bart254 <omondibartlett3@gmail.com>
2022-10-15 07:34:03 -0700

    File that pints numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*This program prints all numbers from 0 to 9
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int digit = 0;
+
+while (digit < 10)
+{printf("%d", digit);
+digit++;
+}
+return (0);
+}
