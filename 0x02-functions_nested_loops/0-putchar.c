14e398f537946e317f4bcf6c25818429d9452295
Bart254 <omondibartlett3@gmail.com>
2022-10-19 13:02:41 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* main - Entry point
+* Prints using __putchar() function
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+_putchar();
+return (0);
+}
