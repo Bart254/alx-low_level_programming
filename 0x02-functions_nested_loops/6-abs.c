8325b6ca5f6ccee70be05de9361f7cd30e3c4968
Bart254 <omondibartlett3@gmail.com>
2022-10-20 07:43:03 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+/**
+* _abs - prints absolute value of int
+* @integer : argument type
+* Return: 0 (success)
+*/
+int _abs(int integer)
+{
+	int absolute = abs(integer);
+
+	return (absolute);
+}
