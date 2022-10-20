f57cc7c139f703cb36a18c302d6dd4be2ab95ec2
Bart254 <omondibartlett3@gmail.com>
2022-10-20 04:33:18 -0700

    betty style modified

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,14 @@
 #include "main.h"
-/* Prints alphabets */
+/**
+* print_alphabet - Entry point
+* Prints alphabets
+* Return:void (None)
+*/
 void print_alphabet(void)
 {
 	char alphabet;
 
-	for(alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
 	_putchar(alphabet);
 	}
