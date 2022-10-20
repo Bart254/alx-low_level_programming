96bf23daad36291c6711a9cce01754c133413e92
Bart254 <omondibartlett3@gmail.com>
2022-10-20 04:10:08 -0700

    pints alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* main - Entry Point
+* Prints lowercase alphabets
+* Return: Always 0 ( Success)
+*/
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
