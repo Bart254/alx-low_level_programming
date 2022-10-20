fe38a2f8eb511fb8f82a68262814e46ee44a79d0
Bart254 <omondibartlett3@gmail.com>
2022-10-20 06:55:51 -0700

    checks lowercases

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include <ctype.h>
+/**
+* _islower - Prints lower for lowercase
+* Return: 1 if lowercase 0 if not
+* @c: argumentcat
+*/
+int _islower(int c)
+{
+	if (c > 'Z')
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
