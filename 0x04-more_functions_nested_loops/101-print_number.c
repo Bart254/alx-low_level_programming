3e15fb6b4a8325a46dafb0e79f2b749abb5f4090
Bart254 <omondibartlett3@gmail.com>
2022-10-22 23:22:31 -0700

    added new character

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -21,20 +21,19 @@ void print_number(int n)
 	_putchar((n % 100) / 10 + '0');
 	_putchar(n % 10 + '0');
 	}
-
 	else if (n >= 100 && n < 1000)
 	{
 	_putchar(n / 100 + '0');
 	_putchar((n % 100) / 10 + '0');
 	_putchar(n % 10 + '0');
 	}
-
 	else if (n >= 10 && n < 100)
 	{
 	_putchar(n / 10 + '0');
 	_putchar(n % 10 + '0');
 	}
-
 	else if (n < 10 && n != 0)
 	_putchar(n % 10 + '0');
+
+	_putchar('\n');
 }
