61ddd03b8886e10c03e2e25a6a197f58c1dfb974
Bart254 <omondibartlett3@gmail.com>
2022-10-22 23:18:58 -0700

    Corrected task 12

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* print_number - prints integers
+* @n: number to be printed
+*/
+void print_number(int n)
+{
+	if (n < 0)
+	_putchar('-');
+
+	else if (n == 0)
+	_putchar(0 + '0');
+
+	n = abs(n);
+
+	if (n >= 1000)
+	{
+	_putchar((n / 1000) + '0');
+	_putchar((n % 1000) / 100 + '0');
+	_putchar((n % 100) / 10 + '0');
+	_putchar(n % 10 + '0');
+	}
+
+	else if (n >= 100 && n < 1000)
+	{
+	_putchar(n / 100 + '0');
+	_putchar((n % 100) / 10 + '0');
+	_putchar(n % 10 + '0');
+	}
+
+	else if (n >= 10 && n < 100)
+	{
+	_putchar(n / 10 + '0');
+	_putchar(n % 10 + '0');
+	}
+
+	else if (n < 10 && n != 0)
+	_putchar(n % 10 + '0');
+}
