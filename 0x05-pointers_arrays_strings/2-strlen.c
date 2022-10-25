925085ee68dd650101613628eafc6b1585fc26b2
Bart254 <omondibartlett3@gmail.com>
2022-10-25 11:27:56 -0700

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,10 @@
+#include <string.h>
+/**
+ * _strlen - Returns the length of a string literal
+ * @s: The string being passed
+ * Return: The length of the passed string
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
