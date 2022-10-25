9a1b85514ad6ee6df834a72e4bc49ed7c86fdf85
Bart254 <omondibartlett3@gmail.com>
2022-10-25 14:09:01 -0700

    Task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include <string.h>
+/**
+ * _strcpy - copies strings
+ * @dest: destination
+ * @src: source
+ * Return: copied string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+
+	return (dest);
+}
