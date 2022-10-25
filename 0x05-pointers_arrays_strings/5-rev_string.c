0d5b850b47283246bfbd7419cceee80fe0446842
Bart254 <omondibartlett3@gmail.com>
2022-10-25 13:14:02 -0700

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - Prints the reverse of words
+ * @s: The string supplied
+ */
+void rev_string(char *s)
+{
+	int i, a;
+
+	a = -1;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	a++;
+	}
+
+	for ( ; a >= 0; a--)
+	{
+	_putchar(s[a]);
+	}
+	_putchar('\n');
+}
