371855d19e2d7fa7369683d7cff38a3aeeee4fd2
Bart254 <omondibartlett3@gmail.com>
2022-10-27 11:15:29 -0700

    Task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include <string.h>
+
+/**
+ * _strcat - This program concatenates strings
+ * @dest: Where the string is added to
+ * @src: The string being added
+ * Return: The final concatenated strings
+ */
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
+
