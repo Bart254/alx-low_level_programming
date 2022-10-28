6cca0e816f44f5e1486c38c1efa2def2813afa79
Bart254 <omondibartlett3@gmail.com>
2022-10-28 14:17:24 -0700

    Task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * string_toupper - Prints the uppercase of the argument
+ * @s: The argument passed
+ * Return: The uppercase
+ */
+char *string_toupper(char *s)
+{
+	int n = 0;
+
+	for ( ; s[n] != '\0'; n++)
+	{
+		if (s[n] > 96 && s[n] < 123)
+			s[n] = s[n] - 32;
+	}
+
+	return (s);
+}
