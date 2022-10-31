61e94f8c944f6a5b3eb9b71e4829b49da3c499f5
Bart254 <omondibartlett3@gmail.com>
2022-10-31 12:43:12 -0700

    coded two functions in one

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,21 @@
-#include "main.h"
 /**
  * main - Prints a quote
  * Return: 1(Successful)
  */
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
 int main(void)
 {
 	char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
