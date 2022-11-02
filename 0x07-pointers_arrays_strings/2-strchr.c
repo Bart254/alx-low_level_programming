8ce0bda3a0cb961a83b93392443325067541fce8
Bart254 <omondibartlett3@gmail.com>
2022-11-02 09:46:00 -0700

    Task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,12 @@
+#include <string.h>
+
+/**
+ * _strchr - locates string char
+ * @s: String
+ * @c: character being located
+ * Return: The string
+ */
+char *_strchr(char *s, char c)
+{
+	return (strchr(s, c));
+}
