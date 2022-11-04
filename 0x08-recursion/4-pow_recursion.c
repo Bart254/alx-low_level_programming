7abd5eac00df755b1520ee07feb377ffec84cd1d
Bart254 <omondibartlett3@gmail.com>
2022-11-04 02:59:39 -0700

    Task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+/**
+ * _pow_recursion - returns x power y
+ * @x: number
+ * @y: to power
+ * Return: the solution, -1(y lower than 0)
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
