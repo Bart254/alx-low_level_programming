41875e71188dcf5c7fe28d3d15a787608c6de71c
Bart254 <omondibartlett3@gmail.com>
2022-11-07 10:13:09 -0800

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the program's name
+ * @argc: number of arguments passed
+ * @argv:  the vector
+ * Return: 0 (successful)
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+concepts of argc and argv
