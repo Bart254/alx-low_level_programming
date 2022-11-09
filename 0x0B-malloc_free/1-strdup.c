142d25f7696e8fe52e1d9134beec77977d13bc01
Bart254 <omondibartlett3@gmail.com>
2022-11-09 04:50:16 -0800

    Task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,11 @@
+#include <string.h>
+
+/**
+ * _strdup- duplicates a string
+ * @str: string being duplicated
+ * Return: string containig the duplicate
+ */
+char *_strdup(char *str)
+{
+	return (strdup(str));
+}
