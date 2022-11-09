bcd45440e71b18b51c2ddd1d5810208aeed8da2f
Bart254 <omondibartlett3@gmail.com>
2022-11-09 13:14:04 -0800

    casted char into malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	s = malloc(size * sizeof(char));
+	s = (char *) malloc(size * sizeof(char));
 	for (e = 0; e < size; e++)
 		s[e] = c;
 	return (s);
