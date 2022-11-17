f1e6e8ad6de0311d9b9f2b72a1df46f2def6edff
Bart254 <omondibartlett3@gmail.com>
2022-11-17 08:19:56 -0800

    Redo the code

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,27 +8,24 @@ int main(void)
 {
 	long int a, b, c;
 
-	int n = 98;
+	int n = 96;
 
 	a = 1;
 	b = 2;
-	c = 0;
 
+	printf("%ld, %ld, ", a, b);
 
 	while (n > 0)
 	{
-		if (c == a + b)
-		{
-			a = b;
-			b = c;
-			if (n == 1)
-				printf("%ld\n", c);
-			else
-				printf("%ld, ", c);
-			n--;
-		}
-		c++;
+		c = a + b;
+		a = b;
+		b = c;
+		if (n == 1)
+			printf("%ld\n", c);
+		else
+			printf("%ld, ", c);
+		n--;
 	}
+
 	return (0);
 }
-
