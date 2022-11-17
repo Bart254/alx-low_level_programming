a564134a69ecc2a5784983ec1d35f4b0a6b0eea3
Bart254 <omondibartlett3@gmail.com>
2022-11-17 02:47:36 -0800

    Make function comply with betty style

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,9 +1,12 @@
 /**
  * print_name - prints the name passed to it
  * @name: name passed
- * @@f: function pointer
+ * @f: function pointer
+ * @n: char array accepted by functions
+ *
+ * Return: nothing
  */
-void print_name(char *name, void (*f)(char *))
+void print_name(char *name, void (*f)(char *n))
 {
 	f(name);
 }
