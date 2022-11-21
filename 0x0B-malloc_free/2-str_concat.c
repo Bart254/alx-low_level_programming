8a0a2c70f9e6c18a5450a90d044d1e9637976751
Bart254 <omondibartlett3@gmail.com>
2022-11-21 10:04:03 +0300

    Remove main.h header file There was a compilation error because I included a header file that doesn't exist

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,28 +12,22 @@ char *str_concat(char *s1, char *s2)
 	unsigned int e1, e2,ls1, ls2;
 	char *s3;
 
-	if (s1 == NULL)
-		ls1 = 1;
-	else
-		ls1 = strlen(s1);
-	if (s2 == NULL)
-		ls2 = 1;
-	else
-		ls2 = strlen(s2);
+	ls1 = strlen(s1);
+	ls2 = strlen(s2);
 	s3 = malloc((ls1 + ls2 + 1) * sizeof(char));
 	if (s3 == NULL)
 		return (NULL);
 	for (e1 = 0; e1 < ls1; e1++)
 	{
 		if (s1 == NULL)
-			s3[e1] = ""[0];
+			break;
 		else
 			s3[e1] = s1[e1];
 	}
 	for (e2 = 0; e2 < ls2; e1++, e2++)
 	{
 		if (s2 == NULL)
-			s3[e1] = ""[0];
+			break;
 		else
 			s3[e1] = s2[e2];
 	}
