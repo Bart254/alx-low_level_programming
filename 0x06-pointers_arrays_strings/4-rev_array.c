a729d2694d49d8b407109afe7327a1c13411030c
Bart254 <omondibartlett3@gmail.com>
2022-11-22 16:23:12 +0300

    Redo code I thought the code would need printing, turns out printing is done with a test code

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -6,35 +6,14 @@
  */
 void reverse_array(int *a, int n)
 {
-	for (n = n - 1; n >= 0; n--)
+	int f, l, b, c;
+
+	for (b = 0, c = n - 1; b < c; b++, c--)
 	{
-		if (n == 0)
-		_putchar(a[0] + '0');
-		else
-		{
-		if (a[n] > 999)
-		{
-		_putchar(a[n] / 1000 + '0');
-		_putchar((a[n] % 1000) / 100 + '0');
-		_putchar((a[n] % 100) / 10 + '0');
-		_putchar((a[n] % 10) + '0');
-		}
-		else if (a[n] > 99)
-		{
-		_putchar(a[n] / 100 + '0');
-		_putchar((a[n] % 100) / 10 + '0');
-		_putchar(a[n] % 10 + '0');
-		}
-		else if (a[n] > 9)
-		{
-		_putchar(a[n] / 10 + '0');
-		_putchar(a[n] % 10 + '0');
-		}
-		else
-		_putchar(a[n] % 10 + '0');
-		_putchar(',');
-		_putchar(' ');
-		}
+		f = a[b];
+		l = a[c];
+
+		a[b] = l;
+		a[c] = f;
 	}
-	_putchar('\n');
 }
