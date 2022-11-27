9d30859b052edef92dc64904295f3d36b4a61620
Bart254 <omondibartlett3@gmail.com>
2022-11-27 06:47:02 +0000

    Rotate string elements by 13 The task was to come up with a program that prints the 13th letter from a string letter The program does not alter special characters, only the alphabet letters

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rot13 - rotates a number by 13 places
+ * @s: string being rotated
+ *
+ * The below program puts the 13th letter from the alphabet in the string
+ * It does not change special characters
+ *
+ * Return: Altered string values
+ */
+char *rot13(char *s)
+{
+	char *p = s;
+
+	for (; *p !='\0'; p++)
+	{
+		for(; (*p >= 'a' && *p <= 'z') || (*p >= 'A' && *p<= 'Z');)
+		{
+			if ((*p > 'M' && *p < 'a') || (*p > 'm'))
+				*p -= 13;
+			else
+				*p += 13;
+			break;
+		}
+	}
+	return (s);
+}
+
