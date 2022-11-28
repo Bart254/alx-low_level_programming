119e6e120a9e67eddc6d2633629812471bb8d8bc
Bart254 <omondibartlett3@gmail.com>
2022-11-28 09:54:58 +0000

    Update header file in lists Added the prototype of add_node Add code for creating nodes

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * add_note - Adds a note element
+ * @head: pointer to head pointer
+ * @str: string to be assigned
+ *
+ * Return: node element pointer
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return(NULL);
+	new->str = strdup(str);
+	new->len = strlen(new->str);
+	new->next = *head;
+	*head = new;
+	return (*head);
+}
+
