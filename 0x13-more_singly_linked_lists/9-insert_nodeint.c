9cff3f2bdfa9bb9e06b6fd6afe2472f41fb56c61
Bart254 <omondibartlett3@gmail.com>
2022-12-01 12:00:22 +0000

    Rewrite position of if statements

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,12 +19,14 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
+	if (idx == 0)
+		++idx;
 	while (position < idx)
 	{
+		if (ptr == NULL)
+			return (NULL);
 		if (position == idx - 1)
 		{
-			if (!ptr)
-				return (NULL);
 			new->next = ptr->next;
 			ptr->next = new;
 		}
