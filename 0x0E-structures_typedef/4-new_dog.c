73b180054ea24e43cff9e3dd7635601f8c6d1f80
Bart254 <omondibartlett3@gmail.com>
2022-12-02 18:21:49 +0000

    Add multiple functions

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * new_dog - creates a new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner
+ *
+ * Return: pointer to new dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *ptr;
+
+	ptr = malloc(sizeof(dog_t));
+	if (ptr == NULL)
+		return (NULL);
+	ptr->name = strdup(name);
+	ptr->age = age;
+	ptr->owner = strdup(owner);
+	return (ptr);
+}
