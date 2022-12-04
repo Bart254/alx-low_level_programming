3fc3b0f2d8fd09f45ef1398bb44927fcf1205b90
Bart254 <omondibartlett3@gmail.com>
2022-12-04 13:19:20 +0000

    Update code I have resized the memory allocation Used free in a loop to remove all memory when function fails

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,14 +14,26 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	array = malloc(width * sizeof(int *));
+	array = malloc(height * sizeof(int *));
 	if (array == NULL)
 		return (NULL);
 	for (r = 0; r < height; r++)
 	{
 		array[r] = malloc(width * sizeof(int));
-		if (array[r] == NULL)
+		if (array[r] == NULL && r == 0)
+		{
+			free (array);
 			return (NULL);
+		}
+		else if (array[r] == NULL && r > 0)
+		{
+			for (--r; r >= 0; r--)
+			{
+				free(array[r]);
+			}
+			free(array);
+			return (NULL);
+		}
 		for (c = 0; c < width; c++)
 			array[r][c] = 0;
 	}
