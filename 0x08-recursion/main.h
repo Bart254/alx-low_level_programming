75f34bc5f873e4fb0d23cdd1bc2b6f7fe799640d
Bart254 <omondibartlett3@gmail.com>
2023-01-16 10:27:36 +0000

    Separate commands in line 2

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,5 +8,10 @@ int _pow_recursion(int x, int y);
 void _print_rev_recursion(char *s);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
+int is_palindrome(char *s);
+int _checker(char *str, int e);
+char *asterik_h (char *s2);
+int wildcmp(char *s1, char *s2);
+char *first(char *s1, char *s2);
 
 #endif
