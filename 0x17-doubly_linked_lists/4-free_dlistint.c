b5008d985df77abbe4f8f756734d50ffac2a3d32
Bart254 <omondibartlett3@gmail.com>
2023-01-20 14:12:09 +0300

    Add a function that frees the entire list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_dlistint - frees a doubly linked list
+ * @h: pointer to head
+ */
+void free_dlistint(dlistint_t **h)
+{
+	dlistint_t *ptr;
+
+	while (*h)
+	{
+		ptr = *h;
+		*h = (*h)->next;
+		free(ptr);
+	}
+}
+
