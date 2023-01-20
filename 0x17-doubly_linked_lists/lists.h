a40a3021bed81ef9ced216a5076c0952b4b9154e
Bart254 <omondibartlett3@gmail.com>
2023-01-20 17:22:56 +0300

    Update header file

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -24,5 +24,6 @@ int sum_dlistint(dlistint_t *head);
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 void free_dlistint(dlistint_t *h);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 
 #endif
