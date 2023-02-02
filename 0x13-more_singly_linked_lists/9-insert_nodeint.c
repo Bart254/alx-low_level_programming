3f69275943182836419177a2ecd1049de2b64e09
Bart254 <omondibartlett3@gmail.com>
2023-02-02 14:02:39 +0300

    update function 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,7 +17,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (head == NULL || (*head == NULL && idx != 0))
 		return (NULL);
 	if (idx == 0)
-		return (add_nodeint(head, n));
+		return (add_nodeint1(head, n));
 	tail = *head;
 	while (tail->next)
 	{
@@ -25,7 +25,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		tail_idx++;
 	}
 	if (idx - tail_idx == 1)
-		return (add_nodeint_end(head, n));
+		return (add_nodeint_end2(&tail, n));
 	else if (idx - tail_idx > 1)
 		return (NULL);
 	new = malloc(sizeof(listint_t));
@@ -39,6 +39,62 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		idx--;
 	}
 	new->next = ptr->next;
+	ptr->next = new;
+	return (new);
+}
+
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * add_nodeint - adds node at the beginning of a list
+ * @head: pointer to head
+ * @n: element to be added to new node
+ * Return: address of new node
+ */
+listint_t *add_nodeint1(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	if (head == NULL)
+		return (NULL);
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = *head;
+	*head = new;
+	return (new);
+}
+
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * add_nodeint_end - adds a node at the end of a list
+ * @head: pointer to head
+ * @n: element to be added to node
+ * Return: address of the new node or NULL if failed
+ */
+listint_t *add_nodeint_end2(listint_t **head, const int n)
+{
+	listint_t *new, *ptr;
+
+	if (head == NULL)
+		return (NULL);
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	if (!(*head))
+	{
+		*head = new;
+		return (new);
+	}
+	ptr = *head;
+	while (ptr->next)
+		ptr = ptr->next;
 	ptr->next = new;
 	return (new);
 }
