d92aee6fabd176bdcf04b8ecda3b18701f07a24d
Bart254 <omondibartlett3@gmail.com>
2023-02-02 13:35:16 +0300

    update further

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -11,12 +11,14 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new, *ptr;
 
+	if (head == NULL)
+		return (NULL);
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
 	new->next = NULL;
-	if (!head)
+	if (!(*head))
 	{
 		*head = new;
 		return (new);
--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,9 +9,9 @@ void free_listint2(listint_t **head)
 {
 	listint_t *ptr;
 	
-	if (!head)
+	if (head == NULL)
 		return;
-	*ptr = *head;
+	ptr = *head;
 	while (ptr)
 	{
 		*head = ptr->next;
