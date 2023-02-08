e72e4e7b060ad566849e514762f392e8ed76deb3
Bart254 <omondibartlett3@gmail.com>
2023-02-08 14:25:46 +0300

    add readme, header, and a function

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,43 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+/**
+ * read_textfile - reads a file and outputs its contents
+ * @filename: name of file to be read
+ * @letters: number of letters that should be written
+ * Return: number of letters read
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char *buffer;
+	ssize_t num_read;
+
+	if (!filename)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buffer = malloc(letters * sizeof(char));
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+	num_read = read(fd, buffer, letters);
+	if (num_read == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+	close(fd);
+	if (write(STDOUT_FILENO, buffer, num_read) == -1)
+		return (0);
+	free(buffer);
+	return (num_read);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,13 @@
+Resources
+	read or watch:
+		File descriptors
+		C programming in Linux Tutorial
+man or help:
+	open
+	close
+	read
+	write
+	dprintf
+In this session, we will be handling files in C. This includes: opening, creating, writing and closing files.
+The above resources are aimed to introduce us to file i/o in C language.
+Happy coding ;-)
