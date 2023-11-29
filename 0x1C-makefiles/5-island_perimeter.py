4cf4ffe9a3b2337380a85d382e500cf196a7218b
Bart254 <omondibartlett@gmail.com>
2023-11-29 20:54:24 +0300

    add file 5-island_perimeter.py

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""Module creates function island_perimeter
+"""
+
+
+def island_perimeter(grid):
+    """ Returns the perimeter of a grid island
+
+        arg(list of lists): list made up of equal lists containing 0 or 1
+    """
+    rows, columns = len(grid), len(grid[0])
+    length, col_len = 0, 0
+    width, row_wid = 0, 0
+    for r in range(rows):
+        for c in range(columns):
+            if r == 0 or r == rows - 1 or c == 0 or c == columns - 1:
+                if grid[r][c] != 0:
+                    print("not island")
+                    return
+            else:
+                if grid[r][c] == 1:
+                    if length == 0 and width == 0:
+                        length += 1
+                        width += 1
+                        col_len = c
+                        row_wid = r
+                    elif grid[r][c - 1] == 1:
+                        if c > col_len:
+                            length += 1
+                            col_len = c
+                    elif grid[r - 1][c] == 1:
+                        if r > row_wid:
+                            width += 1
+                            row_wid = r
+                    else:
+                        print("Two islands")
+                        return
+                if grid[r][c] == 0:
+                    if grid[r - 1][c] == 1 and grid[r + 1][c] == 1:
+                        if grid[r][c + 1] == 1 and grid[r][c - 1] == 1:
+                            print("has a lake")
+                            return
+    perimeter = 2 * (length + width)
+    return perimeter
